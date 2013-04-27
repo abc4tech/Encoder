cfe427b314007194dcf91af9c5d2c0b78495e020
Joakim Børlum <joakim@roevhat.dk>
2013-04-27 17:07:18 +0200

    Go go

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 See
     test_example.ino
 For how to use it.
+
+#License
+
+    "THE BEER-WARE LICENSE" (Revision 42):
+    MJC wrote this file. As long as you retain this notice you
+    can do whatever you want with this stuff. If we meet some day, and you think
+    this stuff is worth it, you can buy us a beer in return.
+    /MJC; Mikkel, Joakim & Casper
--- src/Encoder.h
+++ src/Encoder.h
@@ -4,6 +4,16 @@ Created by MJC; Mikkel, Joakim & Casper
 April 2013
 */
 
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * MJC wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy us a beer in return.
+ * /MJC; Mikkel, Joakim & Casper
+ * ----------------------------------------------------------------------------
+ */
+
 #ifndef Encoder_h
 #define Encoder_h
 
